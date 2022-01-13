ac63d0f9b110f46be2c71dee93a6de24b8a4da93
kkingsbe <kkingsbe@gmail.com>
2022-01-12 22:32:50 -0500

    Fixed wording

--- TechT/src/components/Category.jsx
+++ TechT/src/components/Category.jsx
@@ -8,8 +8,8 @@ const Category = ({title}) => {
                 <ArticleCard title={"LORA Radio"} description={"An introduction to using the RYLR-896 LORA radio with either an Arduino or a Windows computer, including the initial connection, configuration, and sending messages."}></ArticleCard>
                 <ArticleCard title={"MPU6050"} description={"Learn how to use the MPU6050 gyroscope & accelerometer module with an Arduino."}></ArticleCard>
                 <ArticleCard title={"Adafruit Adalogger"} description={"An introduction to using the SD card reader to read and write from an SD card with the Adafruit Adalogger."}></ArticleCard>
-                <ArticleCard title={"Ultimate GPS Module"} description={"How to use the Adafruit Ultimate GPS Module with an Arduino."}></ArticleCard>
-                <ArticleCard title={"WS2812B LED"} description={"Learn how to control WS2812B LED's from an Arduino and create non-blocking animations."}></ArticleCard>
+                <ArticleCard title={"Ultimate GPS Module"} description={"How to use the Adafruit Ultimate GPS Module with an Arduino to get real-time location, altitude, speed, and direction."}></ArticleCard>
+                <ArticleCard title={"Neopixel"} description={"Learn how to control Neopixel (WS2812B) LED's from an Arduino and create non-blocking animations."}></ArticleCard>
                 <ArticleCard title={"OLED Display"} description={"Display text on an OLED display from an Arduino."}></ArticleCard>
                 <ArticleCard title={"Brushless Motor Control"} description={"Control a Brushless motor from an Arduino with the use of an ESC."}></ArticleCard>
             </div>
